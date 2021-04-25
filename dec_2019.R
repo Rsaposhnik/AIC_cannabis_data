34531b3e13f1ca359e0b9bbcbbe41d82e856faac
Rsaposhnik <83136356+Rsaposhnik@users.noreply.github.com>
2021-04-24 18:40:08 -0700

    standardized state names using lat/lon data

--- dec_2019.R
+++ dec_2019.R
@@ -42,7 +42,7 @@ rm(dispensaries_sf,
    dispensaries_delivery)
 
 
-# A2: ADD IN COUNTY-MAPPING TO MERCHANT DETAILS ACROSS USA -----------------------------------------------------
+# A2: ADD IN COUNTY-MAPPING TO MERCHANT DETAILS ACROSS USA (USING LAT/LON)-----------------------------------------------------
 
 ########################### A2.i - Map all US ZIP codes to county subdivisions (subs)
 
@@ -107,6 +107,12 @@ rm(disp_lat_lon)
 #Remove geometry
 mapped_dispensaries <- st_set_geometry(mapped_dispensaries, NULL)
 
+#Remove duplicates Merge IDs (multiple of the same dispensary in the same county)
+mapped_dispensaries <- mapped_dispensaries %>%
+  mutate(dupe = duplicated(id_for_merge)) %>%
+  filter(dupe == FALSE) %>%
+  select(-dupe)
+
 ########################### A2.iii - merge county names back onto the master "all dispensary" df using our merge ID
 all_dispensary_info <- all_dispensary_info %>%
   left_join(.,
@@ -116,15 +122,57 @@ all_dispensary_info <- all_dispensary_info %>%
 rm(mapped_dispensaries)
 
 
+# A3: STANDARDIZE STATE NAME ACROSS DATASET (USING SAME LAT/LON FROM DISPENSARIES) -----------------------------------------------------
 
+########### A3.i Read in Geospatial map of states across USA, and select state Abbrev and geometry
 
+states_geom_df <- states(cb = FALSE, resolution = "500k", year = 2019) %>%
+  select(STUSPS, geometry)
 
-# A3: STANDARDIZE STATE NAME ACROSS DATASET  -----------------------------------------------------
+########### A3.ii #Narrow down dispensary dataset only to geosaptial details and then join on state boundaries
 
+disp_lat_lon <- all_dispensary_info %>%
+  select(ID, id_for_merge, Slug, Longitude, Latitude )
 
+#Convert Lat/Lon to geometry 
+library(sf)
+disp_lat_lon <- st_as_sf(disp_lat_lon, coords = c("Longitude", "Latitude"), 
+                         crs = 4326, agr = "constant")
 
+disp_lat_lon <- st_transform(disp_lat_lon, 4269) 
 
+# Using our greater state-geometry map from earlier, we can now pinpoint (by joining) where each
+# dispensary falls into a specific state
+library(sf)
+mapped_dispensaries <- st_join(disp_lat_lon, states_geom_df, join = st_within) %>%
+  select(id_for_merge, STUSPS) %>%
+  rename("corrected_state" = "STUSPS")
 
+rm(disp_lat_lon)
+
+#Remove geometry
+mapped_dispensaries <- st_set_geometry(mapped_dispensaries, NULL)
+
+#Remove duplicates Merge IDs (multiple of the same dispensary in the same state)
+mapped_dispensaries <- mapped_dispensaries %>%
+  mutate(dupe = duplicated(id_for_merge)) %>%
+  filter(dupe == FALSE) %>%
+  select(-dupe)
+
+########################### A3.iii - merge corrected state names back onto the master "all dispensary" df using our merge ID
+all_dispensary_info <- all_dispensary_info %>%
+  left_join(.,
+            mapped_dispensaries,
+            "id_for_merge")
+
+rm(mapped_dispensaries)
+
+#Reorganize Data
+all_dispensary_info <- all_dispensary_info %>% 
+  select(State,
+         corrected_state,
+         county_name,
+         everything())
 
 
 
